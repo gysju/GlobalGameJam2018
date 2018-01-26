3e3b880edd3047f0663f67953c72084217f859c8
Chloroflex <marcandella.boris@gmail.com>
2018-01-26 23:09:16 +0100

    VWALA ON A FINI LE JEU

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -11,6 +11,28 @@ public class Point: MonoBehaviour{
         transform.position = pos;
     }
 
+
+
+
+    public Point getMostAccurateDestinaton(Vector3 targetDirection)
+    {
+        Point bestPoint = null;
+        float currentDot = -2;
+        float bestPointDot = -2;
+        foreach (Link l in _Links)
+        {
+            Point potentialDest = l.getOtherPoint(this);
+            currentDot = Vector3.Dot(targetDirection, (potentialDest.transform.position - transform.position).normalized);
+            if (bestPointDot < currentDot)
+            {
+                bestPoint = potentialDest;
+                bestPointDot = currentDot;
+            }
+        }
+        return bestPoint;
+    }
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
