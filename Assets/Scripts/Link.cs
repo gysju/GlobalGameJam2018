e3009ca1f05a75aa6fd673e8b582674883c9b226
Chloroflex <marcandella.boris@gmail.com>
2018-01-26 21:56:30 +0100

    Adde basic classes

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="Link", menuName = "Level", order = 50)]
+public class Link : ScriptableObject {
+
+
+    public List<Point> _Points = new List<Point>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Point", menuName = "Level", order = 50)]
+public class Point : ScriptableObject {
+
+    public List<Link> _Links = new List<Link>();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
